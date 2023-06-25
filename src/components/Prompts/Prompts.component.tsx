321a3e5fef02682070222256e78cbeac4dc1691f
Parth Jadhav <jadhavparth99@gmail.com>
2023-06-25 19:38:20 +0530

    feat: improve search ✅

--- src/components/Prompts/Prompts.component.tsx
+++ src/components/Prompts/Prompts.component.tsx
@@ -1,16 +1,24 @@
 import Prompt from "../PromptButton/PromptButton.component";
-import { IPrompt } from "../Search/Search.component";
+
+export interface IPrompt {
+    prompt: string;
+    promptName: string;
+    id: string;
+}
 
 const Prompts = ({ prompts }: { prompts: IPrompt[] }) => {
     return (
         <div style={{
             padding: "5px 10px 10px 10px",
         }}>
-            {prompts.slice(0, 9).map((prompt, index) => (
-                <Prompt key={index} number={index + 1} title={prompt.promptName} text={prompt.prompt} onClick={() => {
-                    console.log(prompt.id);
-                }} />
-            ))}
+            {prompts.length === 0
+                ? <p style={{ color: "#7D7A75", fontSize: "18px", fontWeight: 500, textAlign: "center", }}>No prompts found</p>
+                : prompts.slice(0, 9).map((prompt, index) => (
+                    <Prompt key={index} number={index + 1} title={prompt.promptName} text={prompt.prompt} onClick={() => {
+                        console.log(prompt.id);
+                    }} />
+                ))
+            }
         </div>
     )
 }
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,61 +1,61 @@
 import Database from "tauri-plugin-sql-api";
-import { IPrompt } from "../components/Search/Search.component";
+import { IPrompt } from "../components/Prompts/Prompts.component";
 
 
 // TODO: Add UUID, created_at, last_used_at
 export const createPromptsTable = async () => {
-    const db = await Database.load("sqlite:prompts.db");
-    const createTableQuery = `
+  const db = await Database.load("sqlite:prompts.db");
+  const createTableQuery = `
     CREATE TABLE IF NOT EXISTS prompts (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       promptName TEXT,
       prompt TEXT
     )
   `;
-    await db.execute(createTableQuery);
-    db.close();
+  await db.execute(createTableQuery);
+  db.close();
 }
 
 export const storePrompt = async (promptName: string, prompt: string) => {
-    const db = await Database.load("sqlite:prompts.db");
-    const insertQuery = `
+  const db = await Database.load("sqlite:prompts.db");
+  const insertQuery = `
     INSERT INTO prompts (promptName, prompt)
     VALUES ('${promptName}', '${prompt}')
   `;
-    await db.execute(insertQuery);
-    db.close();
+  await db.execute(insertQuery);
+  db.close();
 }
 
 export const getPrompts = async (): Promise<IPrompt[]> => {
-    const db = await Database.load("sqlite:prompts.db");
-    const selectQuery = `
+  const db = await Database.load("sqlite:prompts.db");
+  const selectQuery = `
     SELECT * FROM prompts
   `;
 
-    const result: [] = await db.select(selectQuery);
-    db.close();
-    return result;
+  const result: [] = await db.select(selectQuery);
+  db.close();
+  return result;
 }
 
 export const searchPrompts = async (searchTerm: string): Promise<IPrompt[]> => {
-    const db = await Database.load("sqlite:prompts.db");
-    const selectQuery = `
+  const db = await Database.load("sqlite:prompts.db");
+  const selectQuery = `
     SELECT * FROM prompts
     WHERE promptName LIKE '%${searchTerm}%'
+    OR prompt LIKE '%${searchTerm}%'
   `;
-
-    const result: IPrompt[] = await db.select(selectQuery);
-    db.close();
-    return result;
+  const result: IPrompt[] = await db.select(selectQuery);
+  db.close();
+  return result;
 }
 
 export const deletePrompt = async (id: number) => {
-    const db = await Database.load("sqlite:prompts.db");
-    const deleteQuery = `
+  const db = await Database.load("sqlite:prompts.db");
+  const deleteQuery = `
         DELETE FROM prompts
         WHERE id = ${id}
     `;
 
-    await db.execute(deleteQuery);
-    db.close();
+  await db.execute(deleteQuery);
+  db.close();
 }
