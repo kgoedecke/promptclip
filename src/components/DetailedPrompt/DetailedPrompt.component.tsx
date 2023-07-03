c75b0eb77c09561dba70df895aef60894007e932
Parth Jadhav <jadhavparth99@gmail.com>
2023-07-03 20:00:45 +0530

    feat: add categories tags on Prompts

--- src/components/DetailedPrompt/DetailedPrompt.component.tsx
+++ src/components/DetailedPrompt/DetailedPrompt.component.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Tag } from '@chakra-ui/react';
 import { useContext } from 'react';
 import { StarIcon } from '../Icons/StarsIcon';
 import { UsedForIcon } from '../Icons/UsedForIcon';
@@ -7,49 +7,24 @@ import { HeartIcon } from '../Icons/HeartIcon';
 import { deletePrompt, toggleFavorite } from '../../utils/database';
 import { TrashIcon } from '../Icons/TrashIcon';
 import { UpdateContext } from '../../contexts/update.context';
+import { CategoriesContext } from '../../contexts/categories.context';
 
 const DetailedPrompt: React.FC<IPrompt> = ({
-  promptName, prompt, used, isFavorite, uuid,
+  promptName, prompt, used, isFavorite, uuid, category_id,
 }) => {
   const { setUpdate } = useContext(UpdateContext);
+  const { categories } = useContext(CategoriesContext);
   return (
-    <Box
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        backgroundColor: 'var(--lighter-overlay-color)',
-        borderRadius: '10px',
-        padding: '16px',
-        marginTop: '16px',
-      }}
-    >
-      <div
+    <div>
+      <Box
         style={{
           display: 'flex',
           justifyContent: 'space-between',
           flexDirection: 'row',
-        }}
-      >
-        <StarIcon width="24px" height="24px" marginRight="12px" />
-        <div
-          style={{
-            width: '506px',
-          }}
-        >
-          <Text fontWeight="bold" color="white">
-            {promptName}
-          </Text>
-          <Text color="grey" textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">
-            {prompt}
-          </Text>
-        </div>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexDirection: 'row',
+          backgroundColor: 'var(--lighter-overlay-color)',
+          borderRadius: '10px',
+          padding: '16px',
+          marginTop: '16px',
         }}
       >
         <div
@@ -57,44 +32,77 @@ const DetailedPrompt: React.FC<IPrompt> = ({
             display: 'flex',
             justifyContent: 'space-between',
             flexDirection: 'row',
-            marginRight: '24px',
           }}
         >
-          <UsedForIcon width="18px" height="18px" marginRight="4px" />
-          <Text fontSize="14px" color="grey" marginTop="-2px">
-            {used}
-          </Text>
+          <StarIcon width="24px" height="24px" marginRight="12px" />
+          <div
+            style={{
+              width: '506px',
+            }}
+          >
+            <Text fontWeight="bold" color="white">
+              {promptName}
+            </Text>
+            <Text color="grey" textOverflow="ellipsis" overflow="hidden" whiteSpace="nowrap">
+              {prompt}
+            </Text>
+            <Tag size="sm" backgroundColor="var(--lighter-overlay-color)" color="grey" marginTop="8px">
+              {categories.find((category) => category.uuid === category_id)?.name || 'Uncategorized'}
+            </Tag>
+          </div>
         </div>
-        {/* <EditIcon
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              marginRight: '24px',
+            }}
+          >
+            <UsedForIcon width="18px" height="18px" marginRight="4px" />
+            <Text fontSize="14px" color="grey" marginTop="-2px">
+              {used}
+            </Text>
+          </div>
+          {/* <EditIcon
         marginTop="1px"
         cursor="pointer"
         onClick={() => {
           console.log('Hei');
         }}
       /> */}
-        <HeartIcon
-          width="18px"
-          height="18px"
-          color={isFavorite ? 'red' : 'grey'}
-          cursor="pointer"
-          marginRight="24px"
-          onClick={async () => {
-            await toggleFavorite(uuid, isFavorite);
-            setUpdate();
-          }}
-        />
-        <TrashIcon
-          width="18px"
-          height="18px"
-          cursor="pointer"
-          color="grey"
-          onClick={async () => {
-            await deletePrompt(uuid);
-            setUpdate();
-          }}
-        />
-      </div>
-    </Box>
+          <HeartIcon
+            width="18px"
+            height="18px"
+            color={isFavorite ? 'red' : 'grey'}
+            cursor="pointer"
+            marginRight="24px"
+            onClick={async () => {
+              await toggleFavorite(uuid, isFavorite);
+              setUpdate();
+            }}
+          />
+          <TrashIcon
+            width="18px"
+            height="18px"
+            cursor="pointer"
+            color="grey"
+            onClick={async () => {
+              await deletePrompt(uuid);
+              setUpdate();
+            }}
+          />
+        </div>
+      </Box>
+    </div>
+
   );
 };
 export default DetailedPrompt;
